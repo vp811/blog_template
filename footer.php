dc6b7a4362fd8ac87974b0cae0c573e4c81ef4ec
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-07 12:17:32 -0500

    edit body and footer

--- footer.php
+++ footer.php
@@ -31,7 +31,6 @@
             </div>
         </footer>
 
-    </div><!-- End Footer Div-->
     <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <!-- Begin Section Container -->
+<div class="container">
     <section class="row">
         <div class="eight columns">
 
@@ -9,16 +10,21 @@
             			if ( have_posts() ) {
             			    while ( have_posts() ) {
             			        the_post(); ?>
-                                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <div class="posts">
+                                    <div class="background-post">
+                                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                        <h4><?php the_date(); ?></h4>
+                                    </div>
                                 <?php
                                     if ( has_post_thumbnail() ) {
                                         the_post_thumbnail('thumbnail');
                                     }
                                 ?>
                                 <?php the_excerpt(); ?>
-                                <a href="<?php the_permalink(); ?>">Read More</a>
-
+                                <a href="<?php the_permalink(); ?>" id="more">Read More</a>
+                                </div>
                             <?php
+
             			    } // end while
             			} // end if
         			?>
@@ -29,5 +35,5 @@
         </div>
     </section>
 <!-- End Section -->
-
+</div>
 <?php get_footer(); ?>
