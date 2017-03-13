af87b0dea11ed37c223522b2eb3a2cef806262f2
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2017-03-13 12:31:43 -0400

    edit the archive page

--- archive.php
+++ archive.php
@@ -4,31 +4,28 @@
 get_header(); ?>
 
 <div class="row">
-	<div class="twelve columns">
-		<?php if ( have_posts() ) : ?>
-			<h2>Archives</h2>
-			<?php
-			// The Loop
-			while ( have_posts() ) : the_post();?>
-			<!-- data context -->
-				<h3><a href="<?php the_permalink() ?>"> <?php the_title(); ?></a></h3>
-				<?php the_excerpt(); ?>
-			<?php endwhile; ?> <!-- End Loop -->
-        <?php else: ?>
-                <p>Sorry, no posts matched your criteria.</p>
-        <?php endif; ?>
+	<div class="eight columns" id="posts">
+        <h2>Posts</h2>
+    		<?php if ( have_posts() ) : ?>
+
+    			<?php
+    			// The Loop
+    			while ( have_posts() ) : the_post();?>
+    			<!-- data context -->
+                    <div id="individual">
+                        <h3><a href="<?php the_permalink() ?>"> <?php the_title(); ?></a></h3>
+                        <h4><?php the_time('F j, Y'); ?></h4>
+                        <?php the_excerpt(); ?>
+                        <a href="<?php the_permalink(); ?>" id="morepostspage">Read More</a>
+                    </div>
+    			<?php endwhile; ?> <!-- End Loop -->
+            <?php else: ?>
+                    <p>Sorry, no posts matched your criteria.</p>
+            <?php endif; ?>
 	</div>
+    <div class="four columns" id="postside">
+        <?php get_sidebar(); ?>
+    </div>
 </div>
-<div class="row">
-	<div class="twelve columns">
-		<h2>Archives by Month:</h2>
-		<ul>
-			<?php wp_get_archives('type=monthly'); ?>
-		</ul>
-		<h2>Archives by Category:</h2>
-		<ul>
-			 <?php wp_list_categories(); ?>
-		</ul>
-	</div>
-</div>
+
 <?php get_footer(); ?>
